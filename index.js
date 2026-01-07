78538549b4985445e09b9c966555b171539fa82b
NurHossain-nur <nurhossain6434@gmail.com>
2026-01-07 09:27:28 +0600

    solve backend custom api essu

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const admin = require("firebase-admin");
 
 const app = express();
+app.set("trust proxy", true);
+
 const port = process.env.PORT || 5000;
 
 const moment = require('moment-timezone');
 
 // Middleware
-app.use(cors());
-// app.use(express.json());
+// app.use(cors());
+
+app.use(cors({
+  origin: [
+    "https://avijatra.com",
+    "https://www.avijatra.com"
+  ],
+  credentials: true
+}));
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
