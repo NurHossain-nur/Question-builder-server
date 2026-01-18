c7eaec0c130fa4bd556c4d1e5ecc4abf6fb3d63e
NurHossain-nur <nurhossain6434@gmail.com>
2026-01-18 09:35:13 +0600

    fix: reorder CORS middleware and update MongoDB URI configuration

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ const port = process.env.PORT || 5000;
 const moment = require('moment-timezone');
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 
-// app.use(cors({
-//   origin: [
-//     "https://avijatra.com",
-//     "https://api.avijatra.com",
-//     "https://www.avijatra.com"
-//   ],
-//   credentials: true,
-//   allowedHeaders: ['Authorization', 'Content-Type']
-// }));
+app.use(cors({
+  origin: [
+    "https://avijatra.com",
+    "https://api.avijatra.com",
+    "https://www.avijatra.com"
+  ],
+  credentials: true,
+  allowedHeaders: ['Authorization', 'Content-Type']
+}));
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
@@ -41,9 +41,9 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.zmtgsgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.zmtgsgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// const uri = `mongodb://${process.env.DB_ADMIN}:${process.env.DB_PASS}@127.0.0.1:27017/${process.env.DB_NAME}?authSource=admin`;
+const uri = `mongodb://${process.env.DB_ADMIN}:${process.env.DB_PASS}@127.0.0.1:27017/${process.env.DB_NAME}?authSource=admin`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
